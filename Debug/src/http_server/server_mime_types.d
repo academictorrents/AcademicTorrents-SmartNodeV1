a7a07510b63cb196abfa5e8cda66e338d69fede6
JonathanNogueira <Jonathan.Nogueira001@gmail.com>
2014-04-03 19:06:38 -0400

    changed names of the source files

--- Debug/src/http_server/server_mime_types.d
+++
@@ -1,5 +0,0 @@
-src/http_server/server_mime_types.d: \
- ../src/http_server/server_mime_types.cpp \
- /home/nogueira/workspace/AcademicTorrents-SmartNodeV1/include/server_mime_types.hpp
-
-/home/nogueira/workspace/AcademicTorrents-SmartNodeV1/include/server_mime_types.hpp:
--- Debug/src/http_server/subdir.mk
+++ Debug/src/http_server/subdir.mk
@@ -4,31 +4,31 @@
 
 # Add inputs and outputs from these tool invocations to the build variables 
 CPP_SRCS += \
-../src/http_server/server.cpp \
-../src/http_server/server_connection.cpp \
-../src/http_server/server_main.cpp \
-../src/http_server/server_mime_types.cpp \
-../src/http_server/server_reply.cpp \
-../src/http_server/server_request_handler.cpp \
-../src/http_server/server_request_parser.cpp 
+../src/http_server/at_server.cpp \
+../src/http_server/at_server_connection.cpp \
+../src/http_server/at_server_main.cpp \
+../src/http_server/at_server_mime_types.cpp \
+../src/http_server/at_server_reply.cpp \
+../src/http_server/at_server_request_handler.cpp \
+../src/http_server/at_server_request_parser.cpp 
 
 OBJS += \
-./src/http_server/server.o \
-./src/http_server/server_connection.o \
-./src/http_server/server_main.o \
-./src/http_server/server_mime_types.o \
-./src/http_server/server_reply.o \
-./src/http_server/server_request_handler.o \
-./src/http_server/server_request_parser.o 
+./src/http_server/at_server.o \
+./src/http_server/at_server_connection.o \
+./src/http_server/at_server_main.o \
+./src/http_server/at_server_mime_types.o \
+./src/http_server/at_server_reply.o \
+./src/http_server/at_server_request_handler.o \
+./src/http_server/at_server_request_parser.o 
 
 CPP_DEPS += \
-./src/http_server/server.d \
-./src/http_server/server_connection.d \
-./src/http_server/server_main.d \
-./src/http_server/server_mime_types.d \
-./src/http_server/server_reply.d \
-./src/http_server/server_request_handler.d \
-./src/http_server/server_request_parser.d 
+./src/http_server/at_server.d \
+./src/http_server/at_server_connection.d \
+./src/http_server/at_server_main.d \
+./src/http_server/at_server_mime_types.d \
+./src/http_server/at_server_reply.d \
+./src/http_server/at_server_request_handler.d \
+./src/http_server/at_server_request_parser.d 
 
 
 # Each subdirectory must supply rules for building sources it contributes
